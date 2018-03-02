74cc0da5e17c38c63d507450909d9f540f7c72cd
Nicole-Gasperini <nicole.gasperini88@gmail.com>
2018-03-01 20:45:03 -0700

    Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
+
+  resources :comments
+  resources :friendsips, only: [:new, :create, :destroy]
+  resources :likes, only: [:new, :create, :destroy]
+  resources :notifications, only: [:new, :create, :destroy]
+  resources :recipes
+  resources :sessions, only: [:new, :create, :destroy]
+
+  root "recipes#new"
+
+  get 'logout', to: "sessions#destroy", as: 'logout'
+
 end
